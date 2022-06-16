dff16ddbb2cf72c1ea05858e8043731a4c835ff0
beku026 <beksultanbakybekov@gmail.com>
2022-06-16 12:29:21 +0600

    completed the second task

--- task-02/index.js
+++ task-02/index.js
@@ -1,3 +1,9 @@
-function test() {
-    
+let arr = [2, 3, 4, 5];
+function test(num) {
+    for (let i = 0; i < arr.length; i++){
+        if (num % arr[i] == 0){
+            console.log(arr[i]);
+        }
+    }
 }
+test(30);
